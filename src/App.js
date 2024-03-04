484cd2f4bd1458792bcc53ca6d0e78e688dca5a9
Turukmenov <turukmenovv@gmail.com>
2024-03-04 17:24:40 +0600

    homework_6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { useRef, useEffect, useState } from "react"
 
-function App() {
+export default function App() {
+//ПРИМЕР С USEREF
+  const inputRef = useRef(null)
+  const focusInput = () => {
+    inputRef.current.focus()
+  }
+
+//ПРИМЕР С USEEFFECT
+  const [currentTime, setCurrentTime] = useState(new Date());
+  useEffect(() => {
+    const timerID = setInterval(() => {
+      setCurrentTime(new Date())
+    }, 1000)
+
+    return () => {
+      clearInterval(timerID)
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <div style={{display: "flex", justifyContent: "center", marginTop: "200px"}}>
+        {}
+        <input type="text" ref={inputRef} />
+        {}
+        <button style={{backgroundColor: "#b781f3", border: "none", width: "120px", borderRadius: "10px", color: "white", fontSize: "16px"}} onClick={focusInput}>Фокус на поле ввода</button>
+      </div>
 
-export default App;
+      <div style={{display: "flex", justifyContent: "center", marginTop: "30px"}}>
+        <h2>Текущее время:</h2>
+        <h2>{currentTime.toLocaleTimeString()}</h2>
+      </div>
+    </div>
+  )
+}
